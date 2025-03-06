5a93fe2897f0fc7faf52a0f06e1503c8ddb77be4
Your Name <your.email@example.com>
2025-03-06 19:39:11 +0700

    Mô tả thay đổi

---
+++ src/Day2/ASCII.java
@@ -0,0 +1,14 @@
+package Day2;
+
+import java.util.Scanner;
+
+public class ASCII {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Nhập chữ cái từ bàn phím:");
+        String character = scanner.nextLine();
+
+
+    }
+}
