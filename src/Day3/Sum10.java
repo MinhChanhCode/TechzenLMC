ec0c67717953960d4eeca4eccd5c6de2a0151c65
Your Name <your.email@example.com>
2025-03-11 14:25:11 +0700

    Mô tả thay đổi

---
+++ src/Day3/Sum10.java
@@ -0,0 +1,19 @@
+package Day3;
+
+import java.util.Scanner;
+
+public class Sum10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter n: ");
+        int n = scanner.nextInt();
+
+        double sum = 0.0;
+
+        for (int i = 1; i <= n; i++) {
+            sum += 1.0 / i;
+        }
+        System.out.println("Sum is = " + sum);
+    }
+}
